df9c78f2b17c57b98f35edb1e5a3cc54126d267b
benoitlx <benoitleroux41@gmail.com>
2025-01-27 19:56:30 +0100

    feat: lex file from argument

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,20 @@ mod tokenizer;
 
 use tokenizer::Token;
 use logos::Logos;
+use std::fs::File;
+use std::io::prelude::*;
+use std::env;
 
-fn main() {
-    let lex = Token::lexer("A <- 256");
+fn main() -> std::io::Result<()> {
+    let args: Vec<String> = env::args().collect();
+
+    let mut file = File::open(&args[1])?;
+    let mut contents = String::new();
+
+    file.read_to_string(&mut contents)?;
+
+
+    let lex = Token::lexer(contents.as_str());
 
     for result in lex {
         match result {
@@ -13,4 +24,6 @@ fn main() {
             Err(_) => panic!("Err occured"),
         }
     }
+
+    Ok(())
 }
