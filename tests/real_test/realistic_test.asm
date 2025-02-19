eb4165fc9075a4a4c9d4d15e7b6b5f93d3367fa1
benoitlx <benoitleroux41@gmail.com>
2025-02-19 21:37:48 +0100

    fix: integration test for lexer

--- tests/real_test/realistic_test.asm
+++ tests/real_test/realistic_test.asm
@@ -11,7 +11,7 @@ D = A
 A = io_adr
 D = D & *A
 A = wait
-D ; JNE
+D != A
 
 check:
 A = ob_detection_mask
@@ -19,7 +19,7 @@ D = A
 A = io_adr
 D = D & *A
 A = move
-D ; JEQ
+D == *A
 
 turn:
 A = left
@@ -36,5 +36,3 @@ A = move_mask
 D = A
 A = send
 JMP
-D ; this is a comment 
-D
